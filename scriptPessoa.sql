027d92968e1fda89571f375f8992c3d8db848cb0
Wellington Camargo <wellington_balbo_lp@hotmail.com>
2022-06-11 20:22:02 -0300

    Inclusão do script Pessoa

---
+++ scriptPessoa.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE [dbo].[Pessoas](
+	[IdPessoa] [int] IDENTITY(1,1) NOT NULL,
+	[Pessoa] [varchar](50) NOT NULL,
+	[Email] [varchar](30) NULL,
+	[Telefone] [varchar](15) NULL,
+	[EhAtivo] [bit] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IdPessoa] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Pessoas] ADD  DEFAULT ((1)) FOR [EhAtivo]
+GO
+
+
